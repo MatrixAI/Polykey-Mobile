df252e243755e22950a339f413f88a607ce22b16
Gideon Rosales <gideonrosales@gmail.com>
2020-12-22 13:29:17 +0800

    feature: store

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 import Vue from "nativescript-vue";
+import Vuex from "vuex";
 import Home from "./components/Home";
 import router from "./router";
 
-Vue.prototype.$goto = function (to, options) {
-    this.$navigateTo(router(to), options)
-}
+// Init routing
+Vue.prototype.$goto = function(to, options) {
+  this.$navigateTo(router(to), options);
+};
+
+// Install the store
+Vue.use(Vuex);
 
 new Vue({
   render: (h) => h("frame", [h(Home)]),
---
+++ app/store/index.ts
@@ -0,0 +1,8 @@
+import Vuex from "vuex";
+import User from "@/store/modules/User";
+
+export default new Vuex.Store({
+    modules : {
+        User
+    }
+});
